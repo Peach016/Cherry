5ba136b95f9ea04830012a55ba2248fbf0d62802
Peach016 <serpostpostolov@yandex.ru>
2025-06-08 18:33:59 +0900

    Delete test1.py

--- test1.py
+++
@@ -1,9 +0,0 @@
-file_name = input()
-file = open(file_name, "r")
-text = file.read()
-if "print" in text:
-    value = text.split("\"")
-    try:
-        print(value[1])
-    except IndexError:
-        print()
